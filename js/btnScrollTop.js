e18c9fbe528e1cef0adfccc85b4451dce7b46dde
Agustin Corbalan <agustin.corbalan1723@gmail.com>
2020-08-31 12:35:18 -0300

    Scroll to top y enlaces portfolio listos

---
+++ js/btnScrollTop.js
@@ -0,0 +1,9 @@
+var boton = document.getElementById('btnTop')
+
+boton.addEventListener('click', function () {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+})
