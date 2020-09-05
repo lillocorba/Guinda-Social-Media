9415703d71502cea269762f1b56b527af980e570
Agustin Corbalan <agustin.corbalan1723@gmail.com>
2020-09-05 19:04:01 -0300

    Correccion scroll y BG Red

---
+++ form.php
@@ -0,0 +1,16 @@
+<?php
+
+$destinatario = "guindasocialmedia@gmail.com";
+$asunto = "Consulta Guinda Social Media";
+$nombre = $_POST ['nombre'];
+$email = $_POST ['email'];
+$mensaje = $_POST ['mensaje'];
+$remitente = $_REQUEST['email'];
+$header = "From: $remitente";
+$mensajeCompleto = $mensaje . "\n\nNombre: " . $nombre . " / Email: " . $email;
+
+mail($destinatario, $asunto, $mensajeCompleto, $header);
+echo "<script>alert('Correo enviado exitosamente')</script>";
+echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>";
+
+?>
